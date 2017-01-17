49fb1888933c97c5c8d448fb73bf866877d7d953
Jeffson <fengyi_wu@yahoo.com>
2017-01-17 10:59:10 +0800

    Add curl handling

--- code/publish.py
+++ code/publish.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-import os, urlparse
+import os, urlparse, sys
 
 # Define event callbacks
 def on_connect(mosq, obj, rc):
@@ -11,6 +11,10 @@ def on_publish(mosq, obj, mid):
 def on_log(mosq, obj, level, string):
     print(string)
 
+#Phase the command line argument
+param_1 = sys.argv[1]
+param_2 = sys.argv[2]
+
 mqttc = mqtt.Client()
 # Assign event callbacks
 mqttc.on_connect = on_connect
@@ -20,7 +24,7 @@ mqttc.on_publish = on_publish
 #mqttc.on_log = on_log
 
 # Parse CLOUDMQTT_URL (or fallback to localhost)
-url_str = os.environ.get('CLOUDMQTT_URL', 'mqtt://obbzesrv:ZFBjLQuS1h3A@m10.cloudmqtt.com:13198')
+url_str = os.environ.get('CLOUDMQTT_URL', param_1)#'mqtt://obbzesrv:ZFBjLQuS1h3A@m10.cloudmqtt.com:13198')
 url = urlparse.urlparse(url_str)
 
 # Connect
@@ -28,7 +32,7 @@ mqttc.username_pw_set(url.username, url.password)
 mqttc.connect(url.hostname, url.port)
 
 # Publish a message
-mqttc.publish("test", "http://www.thewavsite.com/Birthday/bday02.wav")
+mqttc.publish("test", param_2)#"http://www.thewavsite.com/Birthday/bday02.wav")
 
 #disconnect this client
 mqttc.disconnect();
